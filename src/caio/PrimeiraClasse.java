9ba82c295868cfd3c931fb30408659cd68885cd0
KenjiKenji123 <caiofukue36@gmail.com>
2025-01-06 19:42:45 -0300

    Correção

--- src/caio/PrimeiraClasse.java
+++ src/caio/PrimeiraClasse.java
@@ -4,8 +4,10 @@ public class PrimeiraClasse {
 
 	public static void main(String[] args) {
 		String ambiente = args[0];
-		System.out.println(ambiente);
 
+			System.out.println(ambiente);
+		
+	
 	}
 
 }
